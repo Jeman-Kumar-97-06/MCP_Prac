ad02458228532b3bf02e1e7faba8a23b7fee50d9
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-10-16 22:41:12 +0530

    made changes

--- src/server.js
+++ src/server.js
@@ -1,2 +1,16 @@
 import {McpServer} from '@modelcontextprotocol/sdk/server/mcp.js';
-const server = new McpServer({});
+const server = new McpServer({
+    name:"test",
+    version:"1.0.0",
+    capabilities:{
+        resources:{},
+        tools:{},
+        prompts:{}
+    }
+});
+
+function main(){
+
+};
+
+main();
