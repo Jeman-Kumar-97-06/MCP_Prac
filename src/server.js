96cedc8be2f5a7827833efa2f32dc533bc529ba4
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-10-17 12:15:53 +0530

    made changes

--- src/server.js
+++ src/server.js
@@ -1,16 +1,50 @@
 import {McpServer} from '@modelcontextprotocol/sdk/server/mcp.js';
+import {StreamableHTTPServerTransport} from '@modelcontextprotocol/sdk/server/streamableHttp.js';
+
+const exp = require('express');
+const z   = require('zod');
+
+//creating a mcp server:
 const server = new McpServer({
     name:"test",
-    version:"1.0.0",
-    capabilities:{
-        resources:{},
-        tools:{},
-        prompts:{}
-    }
+    version:"1.0.0"
 });
 
-function main(){
 
-};
+server.registerTool(
+    'add',
+    {
+        title        :"Addition Tool",
+        description  : "Add two numbers",
+        inputSchema  : {a:z.number(),b:z.number()},
+        outputSchema : {result: z.number()},
+    },
+    async ({a,b}) => {
+        const output = {result: a+b};
+        return {
+            content : [{type:'text',text:JSON.stringify(output)}],
+            structuredContent: output
+        }
+    }
+);
 
-main();
+const app = exp();
+app.use(exp.json());
+
+app.post('/mcp',async(req,res)=>{
+    const transport = new StreamableHTTPServerTransport({
+        sessionIdGenerator:undefined,
+        enableJsonResponse:true
+    });
+    res.on('close',()=>{
+        transport.close();
+    });
+    await server.connect(transport);
+    await transport.handleRequest(req,res,req.body);
+});
+
+const port = parseInt(process.env.PORT || '3000');
+
+app.listen(port,()=>{
+    console.log(`Demo MCP server running on http://localhost:${port}/mcp`);
+})
